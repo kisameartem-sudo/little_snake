6fa6640a8b69763606ac48b74c76932eeb579150
theBASSia <kisameartem@gmail.com>
2026-09-21 00:14:30 +0300

    Добавлена змея и фрукты

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from color_shema import color_manager
 from field import Field
+from snake import Snake
+from fruit import Fruits
 
 
 class Game:
@@ -13,9 +15,20 @@ class Game:
         self.running = True
         self.field = Field(600)
 
+        self.snake = Snake(15,
+                           15,
+                           self.field.num_cells)
+        self.fruits = Fruits(self.field.num_cells,
+                             set(self.snake.get_snake()))
+
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('My_Snake')
         self.clock = pygame.time.Clock()
+
+        self.moving = {
+            'move_timer': 0,
+            'move_interval': 0.3
+        }
 
     # -------------------------------------------------
     # Обработка событий нажатия кнопок
@@ -30,6 +43,14 @@ class Game:
                 '''Полверка нажатий кнопок'''
                 if event.key == pygame.K_ESCAPE:
                     self.running = False
+                if event.key == pygame.K_w:
+                    self.snake.update_direction('UP')
+                if event.key == pygame.K_d:
+                    self.snake.update_direction('RIGHT')
+                if event.key == pygame.K_a:
+                    self.snake.update_direction('LEFT')
+                if event.key == pygame.K_s:
+                    self.snake.update_direction('DOWN')
 
     # -------------------------------------------------
     # Непрерывный ввод
@@ -40,16 +61,34 @@ class Game:
     # -------------------------------------------------
     # 3. Изменение состояния
     # -------------------------------------------------
-    def update(self):
-        ...
+    def update(self, dt):
+        self.moving['move_timer'] += dt
+
+        if self.moving['move_timer'] >= self.moving['move_interval']:
+            self.moving['move_timer'] -= self.moving['move_interval']
+
+            snake = set(self.snake.get_snake())
+            snake_head = self.snake.next_head_pos()
+            if snake_head in self.fruits.get_fruits():
+                self.fruits.remove_fruit(snake_head, snake)
+                self.snake.move(True)
+            else:
+                self.snake.move()
+
+            if self.fruits.update_delay():
+                self.fruits.add_fruit(snake)
+
 
     # -------------------------------------------------
     # 4. Отрисовка
     # -------------------------------------------------
     def render(self):
-        self.field.draw_cells()
-        self.screen.fill(color_manager.colors['background'].color_RGB)
-        self.screen.blit(self.field.surface, (200, 0))
+        self.field.draw_cells()  # Рисуем поле
+        self.field.draw_fruits(self.fruits.get_fruits())
+        self.field.draw_snake(self.snake.get_snake())  # Рисуем змею
+
+        self.screen.fill(color_manager.colors['background'].color_RGB)  # Рисуем задник
+        self.screen.blit(self.field.surface, (200, 0))  # Отображаем поле
 
         pygame.display.flip()
 
@@ -59,7 +98,7 @@ class Game:
                 dt = self.clock.tick(self.FPS) / 1000.0
                 self.handle_events()
                 self.handle_input()
-                self.update()
+                self.update(dt)
                 self.render()
 
         finally:
